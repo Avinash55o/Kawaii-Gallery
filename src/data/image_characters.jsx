7de288dd5d5c8c76c13f9a0e4e9ab02a0092d87a
Avinash Boruah <avinashboruah8@gmail.com>
2025-07-26 19:22:20 +0530

    added photo

--- src/data/image_characters.jsx
+++ src/data/image_characters.jsx
@@ -1,77 +1,78 @@
+
 const data=[
     {   "no": 1,
         "name": "dino luffy",
-        "photo": "https://i.pinimg.com/1200x/a6/65/f2/a665f203a902e840d8842ce360025c56.jpg"
+        "photo":"/dragon_luffy.jpg" ,
     },
     {
         "no": 2,
         "name": "bunny luffy",
-        "photo": "https://i.pinimg.com/1200x/ea/0a/a9/ea0aa9798e372fb347d9889a31535fec.jpg"
+        "photo": "/bunny.jpg"
     },
     {
         "no": 3,
         "name": "pig luffy",
-        "photo": "https://i.pinimg.com/736x/6e/75/c2/6e75c2049a4681b9600e36e100ed91ce.jpg"
+        "photo": "pig.jpg"
     },
     {
         "no": 4,
         "name": "tiger luffy",
-        "photo": "https://i.pinimg.com/1200x/b4/1c/e2/b41ce225987c3daac272c6994560ff07.jpg"
+        "photo": "tiger.jpg"
     },
     {
         "no": 5,
         "name": "rat luffy",
-        "photo": "https://i.pinimg.com/1200x/72/74/58/727458c0eb5a0a9563c492b2cf38e141.jpg"
+        "photo": "rat.jpg"
     },
     {
         "no": 6,
         "name": "nami",
-        "photo": "https://i.pinimg.com/1200x/34/f3/b3/34f3b3d63352446c0371493225b57d92.jpg"
+        "photo": "nami.jpg"
     },
     {
         "no": 7,
         "name": "cute law",
-        "photo": "https://i.pinimg.com/1200x/74/4e/65/744e658a8b140486a730974b14f3177a.jpg"
+        "photo": "law.jpg"
     },
     {
         "no": 8,
         "name": "boa hancock",
-        "photo": "https://i.pinimg.com/736x/be/0c/2d/be0c2d392355551832fa6e7a9fd8c01d.jpg"
+        "photo": "boa.jpg"
     },
     {
         "no": 9,
-        "name": "boa hancock",
-        "photo": "https://i.pinimg.com/736x/be/0c/2d/be0c2d392355551832fa6e7a9fd8c01d.jpg"
+        "name": "shinobu",
+        "photo":"shinobu.jpg"
     },
     {
         "no": 10,
-        "name": "boa hancock",
-        "photo": "https://i.pinimg.com/736x/be/0c/2d/be0c2d392355551832fa6e7a9fd8c01d.jpg"
+        "name": "poke-team",
+        "photo": "poke.jpg"
     },
     {
         "no":11,
-        "name": "boa hancock",
-        "photo": "https://i.pinimg.com/736x/be/0c/2d/be0c2d392355551832fa6e7a9fd8c01d.jpg"
+        "name": "zoro",
+        "photo": "zoro.jpg"
     },
     {
         "no":12,
-        "name": "boa hancock",
-        "photo": "https://i.pinimg.com/736x/be/0c/2d/be0c2d392355551832fa6e7a9fd8c01d.jpg"
+        "name": "sanji",
+        "photo": "sanji.jpg"
     },
     {
         "no":13,
-        "name": "boa hancock",
-        "photo": "https://i.pinimg.com/736x/be/0c/2d/be0c2d392355551832fa6e7a9fd8c01d.jpg"
+        "name": "hinata",
+        "photo": "hina.jpg"
     },
     {
         "no": 14,
-        "name": "boa hancock",
-        "photo": "https://i.pinimg.com/736x/be/0c/2d/be0c2d392355551832fa6e7a9fd8c01d.jpg"
+        "name": "ichigo",
+        "photo": "ichigo.jpg"
     },
     {
         "no": 15,
-        "name": "boa hancock",
-        "photo": "https://i.pinimg.com/736x/be/0c/2d/be0c2d392355551832fa6e7a9fd8c01d.jpg"
+        "name": "kanao",
+        "photo": "kanao.jpg"
     },
     {
         "no":16,
